40289991e0f5794d26d161050f62416385c5e5b9
SrnrZ <textverarbeitungsprogramm@web.de>
2024-12-03 22:14:10 +0100

    Update trained_model.py

--- trained_model.py
+++ trained_model.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.metrics import classification_report, confusion_matrix
 
-data = pd.read_csv('xxx/heart_disease_health_indicators_BRFSS2015.csv', delimiter=';')
+data = pd.read_csv('https://raw.githubusercontent.com/SrnrZ/ml_health_app/refs/heads/main/heart_disease_health_indicators_BRFSS2015.csv', delimiter=';')
 
 # 'HeartDiseaseorAttack' as target variable (y)
 features = data.drop('HeartDiseaseorAttack', axis=1)
